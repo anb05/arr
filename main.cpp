30bc0b8d7f6aa7ec9c4ba1ae0bfa210b0fd826d1
bovkun <alexandr05@list.ru>
2019-12-19 12:20:28 +0200

    Reverse of linearization

--- main.cpp
+++ main.cpp
@@ -40,16 +40,20 @@ int main(int argc, char const** argv)
 
                     int offset = r * dimC * dimD * dimF + c * dimD * dimF + d * dimF + f;
 
-                    printf("addr = %i\n", *(addr + offset));
+
+                    printf("addr \t\t= %i\n", *(addr + offset));
+
+                    printf("offset \t\t= %i\n", offset);
                 }
             }
 
         }
     }
 
+    /*
     int arr4D[end];
 
-    /*
+
     int cnt = 0;
 
     for (int r = 0; r < dimR; r++, ++cnt) {
@@ -74,12 +78,75 @@ int main(int argc, char const** argv)
 
     printf("Size = %i\n", end);
 
+    std::cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+    std::cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" << std::endl;
 
+    std::cout << "dimR = " << dimR << '\n';
+    std::cout << "dimC = " << dimC << '\n';
+    std::cout << "dimD = " << dimD << '\n';
+    std::cout << "dimF = " << dimF << '\n';
 
+    int iL = 2870;
+    printf("addr = %i\n", *(addr + iL));
 
+    int coefF = 1;
+    int coefD = coefF * dimF;
+    int coefC = coefD * dimD;
+    int coefR = coefC * dimC;
 
+    int r = 0;
+    int c = 0;
+    int d = 0;
+    int f = 0;
 
-    
+    if (iL < end) {
+        while (coefR < iL) {
+            ++r;
+            iL -=coefR;
+        }
+        while (coefC < iL) {
+            ++c;
+            iL -= coefC;
+        }
+        while (coefD < iL) {
+            ++d;
+            iL -= coefD;
+        }
+        f = iL;
+    }
+
+    printf("arr[%i][%i][%i][%i] = %i;\n",r,c,d,f,arr[r][c][d][f]);
+
+    for (int cnt = 0; cnt < end; cnt++) {
+
+        iL = cnt;
+
+        int r = 0;
+        int c = 0;
+        int d = 0;
+        int f = 0;
+
+        if (iL < end) {
+            while (coefR < iL) {
+                ++r;
+                iL -=coefR;
+            }
+            while (coefC < iL) {
+                ++c;
+                iL -= coefC;
+            }
+            while (coefD < iL) {
+                ++d;
+                iL -= coefD;
+            }
+            f = iL;
+        }
+
+        if (arr[r][c][d][f] != *(addr + cnt))
+            std::cout << "ERROR !!!!!" << std::endl;
+
+    }
+
     return 0;
 }
 
